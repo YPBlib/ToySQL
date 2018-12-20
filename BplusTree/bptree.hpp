f0eac468db72196761ba58ed8d72278539b7a64b
YPBlib <ypb1024@foxmail.com>
2018-12-20 10:33:33 +0800

    重写

---
+++ BplusTree/bptree.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+    multilevel (sparse) index 
+*/
+
+
+template <typename T>
+class bpnode
+{
+
+public:
+    void delete(const T&)();
+}
